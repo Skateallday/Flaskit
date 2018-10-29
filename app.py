da3de91de5a2cba5fd8041df6851367f10aac1d3
Marc Inglis <marcinglis91@hotmail.com>
2018-10-29 21:51:04 +0000

    end of the day

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import *
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/signup/')
+def signup():
+        return render_template("signup.html")
+
+@app.route('/homepage')
+def homepage():
+        return render_template("homepage.html")
+
+
 @app.route('/')
 def index():
-        return 'hello world!'
+        return render_template("index.html")
 
 if __name__ == '__main__':
         app.run(debug=True)
---
+++ var/accounts.sql
@@ -0,0 +1,5 @@
+CREATE TABLE accounts(
+    username text,
+    email text,
+    password text)
+    
