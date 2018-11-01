620966a76a771e0643ab98a25743a453233460d7
Skateallday <marcinglis91@hotmail.com>
2018-11-01 15:16:49 +0000

    Done

--- models.py
+++ models.py
@@ -2,14 +2,14 @@ import sqlite3 as sql
 
 
 def insertUser(username,password):
-    con = sql.connect("flaskit.db")
+    con = sql.connect("instaslam.db")
     cur = con.cursor()
     cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username,password))
     con.commit()
     con.close()
 
 def retrieveUsers(username,password):
-    con = sql.connect("flaskit.db")
+    con = sql.connect("instaslam.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM users WHERE username = username & password = password ")
     users = cur.fetchall()
