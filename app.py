5adf45c75b74eba8e27765b8ab0caef6e2da3054
Marc Inglis <marcinglis91@hotmail.com>
2018-11-06 16:24:10 +0000

    daily

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request, redirect, Response, url_for, session, abort
-import sqlite3
+from flask import Flask, render_template, request, redirect, Response, url_for, session, abort, g
 
+import sqlite3
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import models as dbHandler
 import hashlib
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = os.urandom(24)
 
 # config
 UPLOAD_FOLDER = '/static/uploads/'
@@ -34,23 +38,50 @@ def validate(username, password):
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        completion = validate(username, password)
-        if completion ==False:
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('homepage', username=username))
-    return render_template('login.html', error=error)
+        error = None
+        if request.method == 'POST':
+                session.pop('username', None)
+                session['logged_in'] = False
+                session['username'] = request.form['username']
+                username = str(request.form['username'])
+                password = str(request.form['password'])
+                
+                
+                con = sqlite3.connect('static/User.db')
+                completion = False
+                with con:
+                        c = con.cursor()
+                        try:
+                                sql1 = '''SELECT * FROM USERS WHERE USERNAME=? '''
+                                sql2 = '''SELECT * FROM USERS WHERE PASSWORD=? '''
+                                c.execute(sql1, ['username'])
+                                c.execute(sql2, ['password'])
+                        except sqlite3.IntegrityError as e:
+                                print('wrong?')
+                                error = 'These details are incorrect, please try again!'
+                                return render_template("login.html", error=error)
+                                con.commit()
+                        return redirect(url_for('homepage'))
+        
+                error = 'Invalid Credentials. Please try again.'
+        return render_template('login.html', error=error)
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+        
+        if g.username:
+                username=g.username
+                return redirect(url_for('homepage'))
+        else:
+                return render_template('index.html')
+        
+@app.before_request
+def before_request():
+        g.username = None
+        if 'username' in session:
+                g.username = session['username']
 
 @app.route('/signup/')
 def signup():
@@ -58,6 +89,7 @@ def signup():
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
+        error = None
         if request.method == 'POST':
                 signupUsername = request.form['signupUsername']
                 signupPassword = request.form['signupPassword']
@@ -72,32 +104,48 @@ def register():
                                 sql = '''INSERT INTO USERS (USERNAME, PASSWORD) VALUES(?, ?) '''
                                 c.executemany(sql, newEntry)
                         except sqlite3.IntegrityError as e:
-                                print('sqlite error: ', e.args[0]) # column name is not unique
-                        con.commit()
-                return redirect(url_for('login'))
-        return render_template("signup.html")
+                                error = 'This is already an account, please try again!'
+                                return render_template("signup.html", error=error)
+                                con.commit()
+                        return redirect(url_for('login'))
+                
 
 @app.route('/notifications/')
 def notifications():
-        return render_template("notifications.html")
+        if g.username:
+                username=g.username
+        return render_template("notifications.html", username=g.username)
 
 
 
 @app.route('/homepage/')
-@app.route('/homepage/<string:username>')
-def homepage(username):
-        return render_template("homepage.html", username=username)
+def homepage():
+        if g.username:
+                username=g.username
+        return render_template("homepage.html", username=g.username)
 
 
 @app.route('/upload/', methods=['GET', 'POST'])
 def upload():
-        return render_template("upload.html")
-        
+        if g.username:
+                username=g.username
+        return render_template("upload.html", username=g.username)
+
+@app.route("/logout")
+def logout():
+        message= None
+        session['logged_in'] = True
+        session.clear()
+        message = "You have successfully logged out."
+        return render_template("index.html", message=message)
 
 
 @app.route('/profile/')
 def profile():
-        return render_template("profile.html")
+        if g.username:
+                username=g.username
+        
+        return render_template("profile.html", username=g.username)
 
 
 
